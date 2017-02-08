838cc6bfc50c4fb388d606e9e5510a2d5d1958e5
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-02-07 21:41:41 -0800

    file structure updated

---
+++ index.js
@@ -0,0 +1,24 @@
+// Application entry point
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Set up the express app
+const app = express();
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routing
+require('./server/routes')(app);
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome. Nothing here.',
+}));
+
+// start the server
+app.listen(8000, () => {
+  console.log('Server is running on http://localhost:8000');
+});
