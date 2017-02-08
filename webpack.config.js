fe00d8b91dcb140f8eb6df4b766391cc6ca7de45
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-02-07 21:47:23 -0800

    initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+
+module.exports = {
+  // bundle entry point
+  entry: path.join(__dirname, '/client/src/app.jsx'),
+
+  // the bundle file end point
+  output: {
+    path: path.join(__dirname, '/client/dist/js'),
+    filename: 'app.js',
+  },
+
+  module: {
+
+    // apply loaders to files that meet given conditions
+    loaders: [{
+      test: /\.jsx?$/,
+      include: path.join(__dirname, '/client/src'),
+      loader: 'babel',
+      query: {
+        presets: ["react", "es2015"]
+      }
+    }],
+  },
+
+  // watch for rebundling
+  watch: true
+};
